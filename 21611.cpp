ec6a25aae128e7f552bf8ca2fb96e9a0b7af8247
qkrtjdrbs <notlce@naver.com>
2021-09-03 20:11:48 +0900

    Algol

--- 21611.cpp
+++ 21611.cpp
@@ -1,129 +1,127 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, r, c, Map[50][50], tmp[50][50];
+
+int n, m, Map[50][50], score[4];
 int dx[] = {0,-1,1,0,0};
 int dy[] = {0,0,0,-1,1};
-int tdx[] = {0,1,0,-1};
-int tdy[] = {-1,0,1,0};
-int explode[4];
+int dir[] = {3,2,4,1};
+vector<pair<int, int>> order;
 
-bool flag;
-vector<pair<int, int>> emp;
-vector<pair<int, int>> e;
-vector<pair<int, int>> mul;
-
-void bilzzard(int x, int y, int dir, int dist, int cnt){
-    if(dist == -1) return;
-    emp.push_back({x, y});
-    Map[x][y] = 0;
-    bilzzard(x+dx[dir], y+dy[dir], dir, --dist, ++cnt);
-}
-
-void tornado(int x, int y, int cnt, int maxCnt, int dirChange, int dir){
-    if(x == 0 && y == -1) return;
-    int nx, ny, nd;
-    if(cnt == maxCnt){
-        dir == 3 ? nd = 0 : nd = dir + 1;
-        nx = x + tdx[nd];
-        ny = y + tdy[nd];
-        if(Map[nx][ny])   
-            mul.push_back({nx, ny}); 
-        if(dirChange == 1){
-            tornado(nx, ny, 1, maxCnt+1, 0, nd);
-        } else {
-            tornado(nx, ny, 1, maxCnt, dirChange+1, nd);
+void setOrder(){
+    int x, y, d=0, cnt=1, sum=0;
+    x = y = n / 2;
+    order.push_back({x, y});
+    while(1){
+        for(int i=0;i<cnt;i++){
+            x += dx[dir[d]];
+            y += dy[dir[d]];
+            order.push_back({x, y});
+            if(!x && !y) return;
         }
-    } else {    
-        nx = x + tdx[dir];
-        ny = y + tdy[dir];
-        if(Map[nx][ny])  
-            mul.push_back({nx, ny});
-        tornado(nx, ny, cnt+1, maxCnt, dirChange, dir);
+        sum++;
+        d = d == 3 ? d = 0 : d + 1;
+        if(sum == 2){
+            cnt++;
+            sum = 0;
+        }
     }
 }
 
-void grouping(int x, int y, int cnt, int maxCnt, int dirChange, int dir){
-    if(x == 0 && y == -1) return;
-    int nx, ny, nd;
-    if(cnt == maxCnt){
-        dir == 3 ? nd = 0 : nd = dir + 1;
-        nx = x + tdx[nd];
-        ny = y + tdy[nd];
-        if(Map[nx][ny])   
-            mul.push_back({nx, ny}); 
-        if(dirChange == 1){
-            grouping(nx, ny, 1, maxCnt+1, 0, nd);
-        } else {
-            grouping(nx, ny, 1, maxCnt, dirChange+1, nd);
-        }
-    } else {    
-        nx = x + tdx[dir];
-        ny = y + tdy[dir];
-        if(Map[nx][ny])  
-            mul.push_back({nx, ny});
-        grouping(nx, ny, cnt+1, maxCnt, dirChange, dir);
+void blizzard(int dir, int dist){
+    int x, y;
+    x = y = n / 2;
+    for(int i=0;i<dist;i++){
+        x += dx[dir];
+        y += dy[dir];
+        Map[x][y] = 0;
     }
 }
 
-void findEmpty(){
-    while(!emp.empty()){
+void explosion(){
+    bool flag = 1;
+    int prev = 0;
+    vector<pair<int, int>> seq;
+    while(flag){
+        prev = 0; flag = 0;
+        for(int i=1;i<order.size();i++){
+            int x, y;
+            tie(x, y) = order[i];
+            if(!Map[x][y]) continue;
+            if(!prev) {
+                seq.push_back({x, y});
+                prev = Map[x][y];
+                continue;
+            }
+            if(prev == Map[x][y]) seq.push_back({x, y});
+            else {
+                int len = seq.size();
+                if(len >= 4){
+                    for(auto j : seq) Map[j.first][j.second] = 0;
+                    score[prev] += len;
+                    flag = 1;
+                }
+                seq.clear();
+                seq.push_back({x, y});
+                prev = Map[x][y];
+            }
+        }
+        if(seq.size() >= 4){
+            for(auto x : seq) Map[x.first][x.second] = 0;
+            score[prev] += seq.size();
+        }
+        seq.clear();
+    }
+}
+
+void makeMarble(){
+    int seq = 1, prev = 0, pos = 1, tmp[50][50];
+    memset(tmp, 0, sizeof(tmp));
+    for(int i=1;i<order.size();i++){
         int x, y;
-        tie(x, y) = emp.back();
-        emp.pop_back();
+        tie(x, y) = order[i];
+        if(!Map[x][y]) continue;
+        if(!prev) {
+            prev = Map[x][y];
+            continue;
+        }
+        if(prev == Map[x][y]) seq++;
+        else {
+            int fx, fy, sx, sy;
+            tie(fx, fy) = order[pos];
+            tie(sx, sy) = order[pos+1];
+            tmp[fx][fy] = seq;
+            tmp[sx][sy] = prev;
+            seq = 1;
+            pos += 2;
+            if(pos >= n*n) break;
+            prev = Map[x][y];
+        }
     }
+    if(pos < n*n){
+        int fx, fy, sx, sy;
+        tie(fx, fy) = order[pos];
+        tie(sx, sy) = order[pos+1];
+        tmp[fx][fy] = seq;
+        tmp[sx][sy] = prev;
+    }
+    memcpy(Map, tmp, sizeof(tmp));
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(0);
     cin >> n >> m;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin >> Map[i][j];
-        }
+    setOrder();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) cin >> Map[i][j];
     }
-    int sx = (n+1)/2;
-    int sy = (n+1)/2;
     for(int i=0;i<m;i++){
         int dir, dist;
         cin >> dir >> dist;
-        bilzzard(sx, sy, dir, dist, 1);
-        while(1){
-            flag = false;
-            tornado(sx, sy, 0, 1, 0, 0);
-            int seq = 1;
-            for(int i=1;i<e.size();i++){
-                int x = e[i].first;
-                int y = e[i].second;
-                int px = e[i-1].first;
-                int py = e[i-1].second;
-                if(Map[x][y] == 0) {
-                    continue;
-                }
-                if(Map[x][y] != Map[px][py]){
-                    if(seq >= 4){
-                        for(int j=i-seq;j<i;j++){
-                            int x, y;
-                            tie(x, y) = e[j];
-                            explode[Map[x][y]]++;
-                            Map[x][y] = 0;
-                        }
-                    }
-                    seq = 1;
-                } else {
-                    seq++;
-                    if(seq == 4){
-                        flag = true;
-                    }
-                }
-            }
-            e.clear(); 
-            if(!flag) break;
-        }
+        blizzard(dir, dist);
+        explosion();
+        makeMarble();
     }
     int ans = 0;
-    for(int i=1;i<=3;i++){
-        ans += i * explode[i];
-    }
+    for(int i=1;i<=3;i++) ans += (i * score[i]);
     cout << ans;
 }
