436dd2e1a10a5f372ef78af225dc73321428b7af
qkrtjdrbs <notlce@naver.com>
2021-10-02 19:44:04 +0900

    bfs

--- 9376.cpp
+++ 9376.cpp
@@ -1,39 +1,34 @@
 #include <bits/stdc++.h>
-#define MAX 987654321;
 using namespace std;
-int t, n, m, dist[102][102][3], Visit[102][102], ans=MAX;
+int t, n, m, dist[104][104][3], visited[104][104];
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
 
-char Map[102][102];
+char Map[104][104];
 
 void bfs(int i, int j, int k){
-    priority_queue<tuple<int, int, int, int>> pq;
+    priority_queue<tuple<int, int, int>> pq;
+    memset(visited, 0, sizeof(visited));
     dist[i][j][k] = 0;
-    Visit[i][j] = 1;
-    pq.push({0, i, j, k});
+    visited[i][j] = 1;
+    pq.push({0, i, j});
     while(!pq.empty()){
-        int cost, x, y, z;
-        tie(cost, x, y, z) = pq.top();
+        int cost, x, y;
+        tie(cost, x, y) = pq.top();
         pq.pop();
         cost = -cost;
-        if(dist[x][y][z] > cost){
-            dist[x][y][z] = cost;
-        }
         for(int i=0;i<4;i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(Map[nx][ny] != '*'){
-                if(Map[nx][ny] == '#'){
-                    if(dist[nx][ny][k] > dist[x][y][k] + 1){
-                        pq.push({-(dist[x][y][k] + 1), nx, ny, k});
-                    }
-                } else if(Map[nx][ny] == '.'){
-                    if(dist[nx][ny][k] > dist[x][y][k]){
-                        pq.push({-dist[x][y][k], nx, ny, k});
-                    }
-                }
+            int nc = cost;
+            if(nx < 0 || nx >= n+2 || ny < 0 || ny >= m+2) continue;
+            if(visited[nx][ny] || Map[nx][ny] == '*') continue;
+            if(Map[nx][ny] == '#') nc++;  
+
+            if(dist[nx][ny][k] > nc){
+                pq.push({-nc, nx, ny});
+                visited[nx][ny] = 1;
+                dist[nx][ny][k] = nc;
             }
         }
     }
@@ -46,27 +41,7 @@ int main(){
     while(t--){
         int x1, y1, x2, y2;
         cin >> n >> m;
-        int cnt = 0;
-        for(int i=1;i<=n;i++){
-            string s;
-            cin >> s;
-            for(int j=1;j<=m;j++){
-                dist[i][j][0] = MAX;
-                dist[i][j][1] = MAX;
-                dist[i][j][2] = MAX;
-                Map[i][j] = s[j];
-                if(s[j] == '$'){
-                    if(!cnt){
-                        x1 = i;
-                        y1 = j;
-                        cnt++;
-                    } else {
-                        x2 = i;
-                        y2 = j;
-                    }
-                }
-            }
-        }
+        memset(dist, 0xf, sizeof(dist));
         for(int i=0;i<=m+1;i++){
             Map[0][i] = '.';
             Map[n+1][i] = '.';
@@ -75,9 +50,32 @@ int main(){
             Map[i][0] = '.';
             Map[i][m+1] = '.';
         }
-        bfs(x1, y1, 0);
-        bfs(x2, y2, 1);
-        bfs(0, 0, 2);
+        vector<pair<int, int>> p;
+        p.push_back({0, 0});
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                cin >> Map[i][j];
+                if(Map[i][j] == '$'){
+                    p.push_back({i, j});
+                }  
+            }
+        }
+        for(int i=0;i<3;i++) {
+            bfs(p[i].first, p[i].second, i);
+        }
+        long long ans = LLONG_MAX;
+        for (int i = 0; i < n + 2; i++) {
+			for (int j = 0; j < m + 2; j++) {
+				if (Map[i][j] == '*') continue;
+				long long tmp = 0;
+				for (int k = 0; k < 3; k++) {
+					tmp += dist[i][j][k];
+				}
+				if (Map[i][j] == '#') tmp -= 2;
+				ans = min(ans, tmp);
+			}
+		}
+
         cout << ans << '\n';
     }
 }
