3389245e733bc832e8a876303a23f2a7cb3719b1
qkrtjdrbs <notlce@naver.com>
2022-10-13 17:00:31 +0900

    Impl

--- 23288.cpp
+++ 23288.cpp
@@ -1,45 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, k, Map[21][21], color[21][21], cnt[501];
-int dice[7] = {0,1,2,3,4,5,6};
-int dx[] = {0,1,0,-1};
+int n, m, d, k, x, y, Map[21][21], dice[]={1,2,4,3,5,6};
+int dx[] = {0,-1,0,1};
 int dy[] = {1,0,-1,0};
+bool visited[21][21];
 
-tuple<int, int, int> roll(int x, int y, int d){
-    int nx = x + dx[d]; 
-    int ny = y + dy[d];
-    if(nx < 0 || nx >= n || ny < 0 || ny >= m){
-        d = (d+2) % 4;
-        nx = x + dx[d];
-        ny = y + dy[d];
-    }
-    int tmp[7]; memcpy(tmp, dice, sizeof(dice));
-    if(d == 0){
-        tmp[1] = dice[4]; tmp[3] = dice[1]; tmp[4] = dice[6]; tmp[6] = dice[3];
-    } else if(d == 1){
-        tmp[1] = dice[2]; tmp[2] = dice[6]; tmp[5] = dice[1]; tmp[6] = dice[5];
-    } else if(d == 2){
-        tmp[1] = dice[3]; tmp[3] = dice[6]; tmp[4] = dice[1]; tmp[6] = dice[4];
-    } else {
-        tmp[1] = dice[5]; tmp[2] = dice[1]; tmp[5] = dice[6]; tmp[6] = dice[2];
-    }
-    memcpy(dice, tmp, sizeof(tmp));
-    if(dice[6] > Map[nx][ny]) d = d == 3 ? 0 : d + 1;
-    else if(dice[6] < Map[nx][ny]) d = d == 0 ? 3 : d - 1;
-    return {nx, ny, d};
+void opposite(){
+    if(d == 0) d = 2;
+    else if(d == 1) d = 3;
+    else if(d == 2) d = 0;
+    else d = 1;
 }
 
-int coloring(int x, int y, int c){
-    color[x][y] = c;
+void roll(){
+    int nx = x + dx[d];
+    int ny = y + dy[d];
+    if(nx < 0 || nx >= n || ny < 0 || ny >= m){
+        opposite();
+        x += dx[d];
+        y += dy[d];
+    } else {
+        x = nx;
+        y = ny;
+    }
+    int tmp[6];
+    memcpy(tmp, dice, sizeof(dice));
+    if(d == 0){
+        tmp[3] = dice[0]; tmp[5] = dice[3]; tmp[2] = dice[5]; tmp[0] = dice[2];
+    } else if(d == 2){
+        tmp[2] = dice[0]; tmp[5] = dice[2]; tmp[3] = dice[5]; tmp[0] = dice[3];
+    } else if(d == 3){
+        tmp[4] = dice[0]; tmp[5] = dice[4]; tmp[1] = dice[5]; tmp[0] = dice[1];
+    } else {
+        tmp[1] = dice[0]; tmp[5] = dice[1]; tmp[4] = dice[5]; tmp[0] = dice[4];
+    }
+    memcpy(dice, tmp, sizeof(tmp));
+}
+
+void nextDir(){
+    if(dice[5] > Map[x][y]){
+        d = d == 0 ? 3 : d-1;
+    } else if(dice[5] < Map[x][y]){
+        d = d == 3 ? 0 : d+1;
+    }
+}
+
+int dfs(int xx, int yy){
     int res = 0;
     for(int i=0;i<4;i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx < 0 || nx >= n || ny < 0 || ny >= m ) continue;
-        if(color[nx][ny] || Map[x][y] != Map[nx][ny]) continue;
-        res += 1 + coloring(nx, ny, c);
+        int nx = xx + dx[i];
+        int ny = yy + dy[i];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if(visited[nx][ny] || Map[nx][ny] != Map[x][y]) continue;
+        visited[nx][ny] = 1;
+        res += dfs(nx, ny) + 1;
     }
+    return res;
+}
+
+int score(){
+    memset(visited, 0, sizeof(visited));
+    visited[x][y] = 1;
+    return (dfs(x, y)+1)*Map[x][y];
+}
+
+int simulate(){
+    roll();
+    int res = score();
+    nextDir();
     return res;
 }
 
@@ -49,19 +78,9 @@ int main(){
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++) cin >> Map[i][j];
     }
-    int idx = 1;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(!color[i][j]){
-                cnt[idx] = 1 + coloring(i, j, idx);
-                idx++;
-            }
-        }
-    }
-    int score = 0, dir = 0, x = 0, y = 0;
+    int ans = 0;
     for(int i=0;i<k;i++){
-        tie(x, y, dir) = roll(x, y, dir);
-        score += (cnt[color[x][y]] * Map[x][y]);
+        ans += simulate();
     }
-    cout << score;
+    cout << ans;
 }
