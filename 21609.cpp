b00a1297d7594b70054ddd1132766636eebbbd9a
qkrtjdrbs <notlce@naver.com>
2022-10-14 17:14:06 +0900

    Impl

--- 21609.cpp
+++ 21609.cpp
@@ -1,88 +1,121 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, area, Map[22][22], Visit[21][21], minX=99, minY=99, flag, rainbow, ans;
+
+int n, m, ans, Map[21][21], visited[21][21], visited2[21][21];
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
-priority_queue<tuple<int, int, int, int>> block;
-queue<pair<int, int>> zero;
 
-void dfs(int x, int y, int l){
-    ++area;
-    Visit[x][y] = 1;
-    if(Map[x][y] > 0){
-        flag = 1;
-        if(x < minX){
-            minX = x;
-            minY = y;
-        } else if(x == minX && y < minY){
-            minY = y;
-        }
-    }
-    if(Map[x][y] == 0){
-        zero.push({x, y});
-        rainbow++;
-    } 
-    for(int i=0;i<4;i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(0 <= nx && nx < n && 0 <= ny && ny < n){
-            if(!Visit[nx][ny] && Map[x][y] == 0 && Map[nx][ny] == l){
-                dfs(nx, ny, l);
-            }
-            if(!Visit[nx][ny] && (Map[nx][ny] == 0 || Map[nx][ny] == Map[x][y])){
-                dfs(nx, ny, l);
+void initRainbow(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(!Map[i][j]){
+                visited[i][j] = 0;
             }
         }
     }
 }
 
-void eraseBlock(int x, int y, int l){
-    Visit[x][y] = 1;
-    Map[x][y] = -2;
-    for(int i=0;i<4;i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(0 <= nx && nx < n && 0 <= ny && ny < n){
-            if(!Visit[nx][ny] && Map[x][y] == 0 && Map[nx][ny] == l){
-                eraseBlock(nx, ny, l);
-            }
-            if(!Visit[nx][ny] && (Map[nx][ny] == 0 || Map[nx][ny] == l)){
-                eraseBlock(nx, ny, l);
+int getBiggestGroup(){
+    memset(visited, 0, sizeof(visited));
+    priority_queue<tuple<int, int, int, int, int>> pq;
+    int num=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(!visited[i][j] && Map[i][j] >= 1){
+                int rbw=0, area=0; 
+                queue<pair<int, int>> q;
+                q.push({i, j});
+                visited[i][j] = num;
+                while(!q.empty()){
+                    auto [x, y] = q.front();
+                    q.pop();
+                    area++;
+                    if(!Map[x][y]) rbw++;
+                    for(int k=0;k<4;k++){
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) continue;
+                        if(Map[i][j] == Map[nx][ny] || !Map[nx][ny]){
+                            q.push({nx, ny});
+                            visited[nx][ny] = num;
+                        }
+                    }
+                }
+                if(area >= 2){
+                    pq.push({area, rbw, i, j, num});
+                }
+                initRainbow();
+                num++;
             }
         }
     }
+    if(pq.empty()) return -1;
+    auto [a, b, c, d, e] = pq.top();
+    return e;
 }
 
-void gravity(int x, int y){
-    if(x == n-1) return;
-    if(Map[x+1][y] == -1 || Map[x+1][y] >= 0){
-        return;
+int dfs(int x, int y, int num){
+    int res = 0;
+    for(int k=0;k<4;k++){
+        int nx = x + dx[k];
+        int ny = y + dy[k];
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n || visited2[nx][ny]) continue;
+        if(visited[nx][ny] == num || !Map[nx][ny]){
+            visited2[nx][ny] = 1;
+            Map[nx][ny] = -2;
+            res += 1 + dfs(nx, ny, num);
+        }
     }
-    if(Map[x+1][y] == -2){
-        int tmp = Map[x+1][y];
-        Map[x+1][y] = Map[x][y];
-        Map[x][y] = tmp;
+    return res;
+}
+
+int eraseBlock(int num){
+    int res = 0;
+    memset(visited2, 0, sizeof(visited2));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(visited[i][j] == num && !visited2[i][j]){
+                Map[i][j] = -2;
+                visited2[i][j] = 1;
+                res = 1 + dfs(i, j, num);
+                return res;
+            }
+        }
     }
-    gravity(x+1, y);
+    return res;
+}
+
+void gravity(){
+    for(int j=0;j<n;j++){
+        for(int i=n-1;i>=0;i--){
+            if(Map[i][j] > -1){
+                for(int k=i;k<n-1;k++){
+                    if(Map[k+1][j] == -2){
+                        swap(Map[k][j], Map[k+1][j]);
+                    } else break;
+                }
+            }
+        }
+    }
 }
 
 void rotate(){
     int tmp[21][21];
+    for(int i=0;i<21;i++){
+        for(int j=0;j<21;j++){
+            tmp[i][j] = -2;
+        }
+    }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            tmp[i][j] = Map[i][j];
+            tmp[i][j] = Map[j][n-i-1];
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){          
-            Map[n-(j+1)][i] = tmp[i][j];
-        }
-    }
+    memcpy(Map, tmp, sizeof(tmp));
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(0);
     cin >> n >> m;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -90,55 +123,12 @@ int main(){
         }
     }
     while(1){
-        flag = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(Map[i][j] > 0 && !Visit[i][j]){
-                    area = 0;
-                    rainbow = 0;
-                    minX = 99;
-                    minY = 99;
-                    dfs(i, j, Map[i][j]);
-                    block.push({area, rainbow, minX, minY});
-                    while(!zero.empty()){
-                        int x, y;
-                        tie(x, y) = zero.front();
-                        zero.pop();
-                        Visit[x][y] = 0;
-                    }
-                }
-            }
-        }
-        if(!flag) break;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                Visit[i][j] = 0;
-            }
-        }
-        priority_queue<tuple<int, int, int, int>> pq;
-        int x, y, z, r;
-        tie(z, r, x, y) = block.top();
-        if(z < 2) break;
-        block.swap(pq);
-        eraseBlock(x, y, Map[x][y]);
-        ans += z*z;
-        for(int j=0;j<n;j++){
-            for(int i=n-1;i>=0;i--){
-                Visit[i][j] = 0;
-                if(Map[i][j] >= 0){
-                    gravity(i, j);
-                }
-            }
-        }
+        int num = getBiggestGroup();
+        if(num == -1) break;
+        ans += pow(eraseBlock(num), 2);
+        gravity();
         rotate();
-        for(int j=0;j<n;j++){
-            for(int i=n-1;i>=0;i--){
-                Visit[i][j] = 0;
-                if(Map[i][j] >= 0){
-                    gravity(i, j);
-                }
-            }
-        }
+        gravity();
     }
     cout << ans;
 }
